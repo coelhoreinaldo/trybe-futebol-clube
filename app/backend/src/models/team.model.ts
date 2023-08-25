119efd1fba30e8964bac0558fec2d21d6f485ad6
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-25 15:53:51 -0300

    ✨ feat: create findAll teams method

--- app/backend/src/models/team.model.ts
+++ app/backend/src/models/team.model.ts
@@ -1,6 +1,12 @@
-// import SequelizeTeam from '../database/models/SequelizeTeam';
-// import { ITeamModel } from '../Interfaces/teams/ITeamModel';
+import ITeam from '../Interfaces/teams/ITeam';
+import SequelizeTeam from '../database/models/SequelizeTeam';
+import { ITeamModel } from '../Interfaces/teams/ITeamModel';
 
-// export default class implements ITeamModel {
-//   private model = SequelizeTeam;
-// }
+export default class implements ITeamModel {
+  private model = SequelizeTeam;
+
+  async findAll(): Promise<ITeam[]> {
+    const dbData = this.model.findAll();
+    return dbData;
+  }
+}
