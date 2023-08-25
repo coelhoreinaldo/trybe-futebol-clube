1818e74fbe4a75386f538c7c3c9b0a75d23c3e4c
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-25 15:58:28 -0300

    ✨ feat: create getAllTeams service and service response interface

---
+++ app/backend/src/services/TeamService.ts
@@ -0,0 +1,15 @@
+import { ServiceResponse } from '../Interfaces/ServiceResponse';
+import ITeam from '../Interfaces/teams/ITeam';
+import { ITeamModel } from '../Interfaces/teams/ITeamModel';
+import TeamModel from '../models/TeamModel';
+
+export default class TeamService {
+  constructor(
+    private teamModel: ITeamModel = new TeamModel(),
+  ) {}
+
+  public async getAllTeams(): Promise<ServiceResponse<ITeam[]>> {
+    const allTeams = await this.teamModel.findAll();
+    return { status: 'SUCCESSFUL', data: allTeams };
+  }
+}
