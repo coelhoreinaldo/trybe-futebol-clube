c20069bdcd4a6c99179f75bcd30011bd4201184f
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-25 15:50:59 -0300

    ✨ feat: create CRUD model

---
+++ app/backend/src/models/team.model.ts
@@ -0,0 +1,6 @@
+// import SequelizeTeam from '../database/models/SequelizeTeam';
+// import { ITeamModel } from '../Interfaces/teams/ITeamModel';
+
+// export default class implements ITeamModel {
+//   private model = SequelizeTeam;
+// }
