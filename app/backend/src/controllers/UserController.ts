9ffb8d83262fb10fa29787cdad0b6d3aa191c9e6
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-26 13:49:43 -0300

    ✨ feat: create user controller

---
+++ app/backend/src/controllers/UserController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import UserService from '../services/UserService';
+import mapStatusHTTP from '../utils/mapStatusHTTP';
+
+export default class UserController {
+  constructor(
+    private userService: UserService = new UserService(),
+  ) {}
+
+  public async login(req: Request, res: Response): Promise<Response> {
+    const loginData = req.body;
+    const serviceResponse = await this.userService.login(loginData);
+    return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
+  }
+}
