63603a62fdc9e396e872ac8abcf851cce52ffd45
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-27 12:05:19 -0300

    ✨ feat: getAll fix

--- app/backend/src/models/MatchModel.ts
+++ app/backend/src/models/MatchModel.ts
@@ -1,12 +1,25 @@
 import IMatch from '../Interfaces/matches/IMatch';
 import SequelizeMatch from '../database/models/SequelizeMatch';
 import { IMatchModel } from '../Interfaces/matches/IMatchModel';
+import SequelizeTeam from '../database/models/SequelizeTeam';
 
 export default class implements IMatchModel {
   private model = SequelizeMatch;
 
   public async findAll(): Promise<IMatch[]> {
-    const dbData = this.model.findAll();
+    const dbData = this.model.findAll(
+      {
+        include: [{
+          model: SequelizeTeam,
+          as: 'homeTeam',
+          attributes: ['teamName'],
+        }, {
+          model: SequelizeTeam,
+          as: 'awayTeam',
+          attributes: ['teamName'],
+        }],
+      },
+    );
     return dbData;
   }
 }
--- app/backend/src/tests/mocks/match.mock.ts
+++ app/backend/src/tests/mocks/match.mock.ts
@@ -7,24 +7,136 @@ const matches = [
     awayTeamGoals: 1,
     inProgress: false,
     homeTeam: {
-      teamName: 'São Paulo',
+      teamName: 'Avaí/Kindermann',
+    },
+    awayTeam: {
+      teamName: 'Avaí/Kindermann',
+    },
+  },
+  {
+    id: 2,
+    homeTeamId: 9,
+    homeTeamGoals: 1,
+    awayTeamId: 14,
+    awayTeamGoals: 1,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Bahia',
+    },
+    awayTeam: {
+      teamName: 'Bahia',
+    },
+  },
+  {
+    id: 3,
+    homeTeamId: 4,
+    homeTeamGoals: 3,
+    awayTeamId: 11,
+    awayTeamGoals: 0,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Botafogo',
+    },
+    awayTeam: {
+      teamName: 'Botafogo',
+    },
+  },
+  {
+    id: 4,
+    homeTeamId: 3,
+    homeTeamGoals: 0,
+    awayTeamId: 2,
+    awayTeamGoals: 0,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Corinthians',
+    },
+    awayTeam: {
+      teamName: 'Corinthians',
+    },
+  },
+  {
+    id: 5,
+    homeTeamId: 7,
+    homeTeamGoals: 1,
+    awayTeamId: 10,
+    awayTeamGoals: 1,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Cruzeiro',
+    },
+    awayTeam: {
+      teamName: 'Cruzeiro',
+    },
+  },
+  {
+    id: 6,
+    homeTeamId: 5,
+    homeTeamGoals: 1,
+    awayTeamId: 13,
+    awayTeamGoals: 1,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Ferroviária',
+    },
+    awayTeam: {
+      teamName: 'Ferroviária',
+    },
+  },
+  {
+    id: 7,
+    homeTeamId: 12,
+    homeTeamGoals: 2,
+    awayTeamId: 6,
+    awayTeamGoals: 2,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Flamengo',
+    },
+    awayTeam: {
+      teamName: 'Flamengo',
+    },
+  },
+  {
+    id: 8,
+    homeTeamId: 15,
+    homeTeamGoals: 0,
+    awayTeamId: 1,
+    awayTeamGoals: 1,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Grêmio',
     },
     awayTeam: {
       teamName: 'Grêmio',
     },
   },
   {
-    id: 41,
-    homeTeamId: 16,
-    homeTeamGoals: 2,
-    awayTeamId: 9,
-    awayTeamGoals: 0,
-    inProgress: true,
+    id: 9,
+    homeTeamId: 1,
+    homeTeamGoals: 0,
+    awayTeamId: 12,
+    awayTeamGoals: 3,
+    inProgress: false,
     homeTeam: {
-      teamName: 'São Paulo',
+      teamName: 'Internacional',
     },
     awayTeam: {
       teamName: 'Internacional',
+    },
+  },
+  {
+    id: 10,
+    homeTeamId: 2,
+    homeTeamGoals: 0,
+    awayTeamId: 9,
+    awayTeamGoals: 2,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Minas Brasília',
+    },
+    awayTeam: {
+      teamName: 'Minas Brasília',
     },
   },
 ];
