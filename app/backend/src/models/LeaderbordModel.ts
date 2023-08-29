9148512d8089a9175812d7c00134e4162218dac7
coelhoreinaldo <scoelhor7@gmail.com>
2023-08-29 11:06:50 -0300

    ✨ feat: hometeams order by

--- app/backend/src/models/LeaderbordModel.ts
+++ app/backend/src/models/LeaderbordModel.ts
@@ -13,12 +13,12 @@ const homeTeamStandingsQuery = `
       SUM(CASE WHEN m.home_team_goals < m.away_team_goals THEN 1 ELSE 0 END) AS totalLosses,
       SUM(m.home_team_goals) AS goalsFavor,
       SUM(m.away_team_goals) AS goalsOwn
-   FROM teams AS t
-   INNER JOIN matches AS m
-   ON t.id = m.home_team_id
-   WHERE m.in_progress = 0
-   GROUP BY t.id
-   ORDER BY goalsFavor DESC;
+  FROM teams AS t
+  INNER JOIN matches AS m
+  ON t.id = m.home_team_id
+  WHERE m.in_progress = 0
+  GROUP BY t.id
+  ORDER BY totalPoints DESC, goalsFavor DESC;
 `;
 
 export default class ILeaderboardModel {
